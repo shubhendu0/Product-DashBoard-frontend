eaf90a0795fbc7bf24720a7571b601df7e280a53
Shubhendu Bora <Shubhendu Bora>
2023-01-15 00:15:53 +0530

    first commit

---
+++ src/components/Footer.js
@@ -0,0 +1,11 @@
+import React from 'react';
+
+const Footer=()=>{   
+    return(
+        <div className="footer">
+            <h3 style={{"color":"white"}}> mern app </h3>
+        </div>
+    )
+}
+
+export default Footer;
